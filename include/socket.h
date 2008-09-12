3536deb7b5e7ca9d165ab5c7ecdb3c82633b12dd
brian bulkowski <brian@ptreyes-x804.(none)>
2008-09-12 03:40:44 -0700

    Poor ascli.

--- include/socket.h
+++ include/socket.h
@@ -44,6 +44,7 @@ typedef struct cf_mcastsocket_cfg_t cf_mcastsocket_cfg;
 /* Function declarations */
 extern int cf_socket_set_nonblocking(int s);
 extern int cf_socket_recv(int sock, void *buf, size_t buflen, int flags);
+extern int cf_socket_send(int sock, void *buf, size_t buflen, int flags);
 extern int cf_socket_init_svc(cf_socket_cfg *s);
 extern int cf_socket_init_client(cf_socket_cfg *s);
 extern int cf_mcastsocket_init(cf_mcastsocket_cfg *ms);
