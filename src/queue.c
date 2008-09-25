750878d51fa649e46ae191085482ed12dbff29ff
brian bulkowski <brian@ptreyes-x804.(none)>
2008-09-24 20:26:22 -0700

    Remove a verbosity

--- src/queue.c
+++ src/queue.c
@@ -156,7 +156,6 @@ cf_queue_pop(cf_queue *q, void *buf, int ms_wait)
 		}
 		else if (CF_QUEUE_NOWAIT == ms_wait) {
 			pthread_mutex_unlock(&q->LOCK);
-			D("cf_queue_pop: EMPTY returning %d",CF_QUEUE_EMPTY);
 			return(CF_QUEUE_EMPTY);
 		}
 		else {
