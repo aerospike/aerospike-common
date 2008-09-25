b42aaabd4731beaac16bffe6938955f23d6d619f
Gian-Paolo Musumeci <gp@citrusleaf.net>
2008-09-24 20:21:53 -0700

    don't delete prototypes

--- include/alloc.h
+++ include/alloc.h
@@ -37,6 +37,7 @@ typedef cf_atomic_int cf_rc_counter;
 extern uint64_t cf_rc_count(void *addr);
 extern void *cf_rc_alloc(size_t sz);
 extern int cf_rc_reserve(void *addr);
+extern uint64_t _cf_rc_release(void *addr, bool autofree);
 #define cf_rc_release(a) (_cf_rc_release((a), FALSE))
 #define cf_rc_releaseandfree(a) (_cf_rc_release((a), TRUE))
 extern void cf_rc_free(void *addr);
