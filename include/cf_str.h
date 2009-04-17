92967ab7f1a958322748906a6c18f566ffb04731
Brian Bulkowski <brian@citrusleaf.net>
2009-04-17 13:27:24 -0700

    Stragglers

---
+++ include/cf_str.h
@@ -0,0 +1,21 @@
+/*
+ *  Citrusleaf Foundation
+ *
+ *  Copyright 2008 by Citrusleaf.  All rights reserved.
+ *  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE.  THE COPYRIGHT NOTICE
+ *  ABOVE DOES NOT EVIDENCE ANY ACTUAL OR INTENDED PUBLICATION.
+ */
+#pragma once
+
+
+#include "cf.h"
+
+extern unsigned int cf_str_itoa(int value, char *s, int radix);
+extern unsigned int cf_str_itoa_u64(uint64_t value, char *s, int radix);
+
+// Split the string 'str' based on input breaks in fmt
+// The splitting is destructive
+// The pointers will be added to the end of vector *v
+// the vector better be created with object size 'void *'
+struct vector_s;
+extern void cf_str_split(char *fmt, char *str, struct vector_s *v);
