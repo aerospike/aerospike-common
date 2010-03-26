d4536c6053d3ba43af9de7206865fdb0a97c4576
Brian Bulkowski <brian@citrusleaf.net>
2010-03-26 12:10:54 -0700

    Debug code for digest validation. All turned off in normal cases.

--- include/digest.h
+++ include/digest.h
@@ -11,6 +11,7 @@
 #include <stddef.h>
 #include <openssl/ripemd.h>
 #include <openssl/md4.h>
+#include <stdio.h>
 
 
 /* SYNOPSIS
@@ -21,6 +22,7 @@
  * The actual computation is done in one shot by calling cf_digest_compute().
  */
 
+// #define DEBUG_VERBOSE 1
 
 
 /* cf_digest
@@ -34,6 +36,21 @@ static inline void
 cf_digest_compute(void *data, size_t len, cf_digest *d)
 {
 	RIPEMD160(data, len, (unsigned char *) d->digest);
+
+#ifdef DEBUG_VERBOSE	
+	fprintf(stderr, "digest computation: len %zu\n",len);
+	uint8_t *buf = data;
+	int i;
+	for (i=0;i<len;i++) {
+		fprintf(stderr, "%02x ",buf[i]);
+		if (i % 16 == 15) fprintf(stderr, "\n");
+	}
+	if (i % 16 != 0) fprintf(stderr, "\n");
+	
+	fprintf(stderr, "digest output : %"PRIx64"\n",*(uint64_t *)d);
+#endif	
+
+	
 }
 
 
@@ -49,6 +66,26 @@ cf_digest_compute2(void *data1, size_t len1, void *data2, size_t len2, cf_digest
 	RIPEMD160_Update(&c, data2, len2);
 	RIPEMD160_Final( (unsigned char *)(d->digest), &c);
 	
+#ifdef DEBUG_VERBOSE	
+	fprintf(stderr, "digest computation2: part1 len %zu\n",len1);
+	uint8_t *buf = data1;
+	int i;
+	for (i=0;i<len1;i++) {
+		fprintf(stderr, "%02x ",buf[i]);
+		if (i % 16 == 15) fprintf(stderr, "\n");
+	}
+	if (i % 16 != 0) fprintf(stderr, "\n");
+	
+	fprintf(stderr, "digest computation2: part2 len %zu\n",len2);
+	buf = data2;
+	for (i=0;i<len2;i++) {
+		fprintf(stderr, "%02x ",buf[i]);
+		if (i % 16 == 15) fprintf(stderr, "\n");
+	}
+	if (i % 16 != 0) fprintf(stderr, "\n");
+	
+	fprintf(stderr, "digest output : %"PRIx64"\n",*(uint64_t *)d);
+#endif	
 }
 
 static inline uint32_t cf_digest_gethash(cf_digest *d, uint32_t MASK) 
