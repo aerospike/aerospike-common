a122d9783680537e499744ef714374c5298270cb
Brian Bulkowski <brian@bulkowski.org>
2010-06-29 02:54:57 -0700

    Get the seconds instead of milliseconds

--- include/cf.h
+++ include/cf.h
@@ -215,6 +215,7 @@ extern void cf_process_daemonize();
 typedef uint64_t cf_clock;
 extern cf_clock cf_getms();
 extern cf_clock cf_clock_getabsolute();
+extern cf_clock get_get_seconds();
 
 /* daemon.c */
 extern void cf_process_privsep(uid_t uid, gid_t gid);
