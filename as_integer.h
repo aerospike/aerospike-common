df0882a54e89cd36a68fc8fe32fef65c96af06be
Chris Stivers <chris@aerospike.com>
2012-11-14 12:19:34 -0800

    as_integer is now backed by a int64_t, not a uint64_t.

--- as_integer.h
+++ as_integer.h
@@ -8,11 +8,11 @@ typedef struct as_integer_s as_integer;
 
 int as_integer_free(as_integer *);
 
-as_integer * as_integer_new(uint64_t);
+as_integer * as_integer_new(int64_t);
 
 int as_integer_inc(as_integer *);
 
-uint64_t as_integer_toint(const as_integer *);
+int64_t as_integer_toint(const as_integer *);
 
 as_val * as_integer_toval(const as_integer *);
 
