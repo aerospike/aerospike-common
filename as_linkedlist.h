e117f56d9c6d20c939ed1ff849f43bfbfc0a3dd1
Chris Stivers <chris@aerospike.com>
2012-11-26 20:11:50 -0800

    a linked-list backed list

---
+++ as_linkedlist.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "as_list.h"
+#include "as_val.h"
+
+/**
+ * Create a linked-list backed list
+ * @param the head of the list
+ * @param the tail of the list
+ * @return a new as_list
+ */
+as_list * as_linkedlist_new(as_val *, as_list *);
