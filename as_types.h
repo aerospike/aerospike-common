f57483d0e29bfa4fcec99b00371844335a6df475
Chris Stivers <chris@aerospike.com>
2012-11-26 20:13:30 -0800

    added new types

--- as_types.h
+++ as_types.h
@@ -4,8 +4,14 @@
 #include "as_boolean.h"
 #include "as_integer.h"
 #include "as_string.h"
+
 #include "as_list.h"
+#include "as_arraylist.h"
+#include "as_linkedlist.h"
+
 #include "as_map.h"
+#include "as_hashmap.h"
+
 
 // Type Interfaces
 #include "as_rec.h"
