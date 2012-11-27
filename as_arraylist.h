131b92baecce8aa72295123ecabf3a1cfc788462
Chris Stivers <chris@aerospike.com>
2012-11-26 20:11:36 -0800

    "array" (really a buffer) backed list

---
+++ as_arraylist.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "as_list.h"
+
+/**
+ * Create a "array" backed list
+ * @param initial size of the buffer
+ * @param size for each capacity increase
+ * @return a new as_list
+ */
+as_list * as_arraylist_new(uint32_t, uint32_t);
