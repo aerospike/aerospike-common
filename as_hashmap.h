72d0d246fca356f406b5ec7be041937d910e679f
Chris Stivers <chris@aerospike.com>
2012-11-28 19:20:02 -0800

    added first map implementation: hashmap, which is backed by shash.

--- as_hashmap.h
+++ as_hashmap.h
@@ -4,9 +4,8 @@
 
 /**
  * Create a hashmap backed map
- * @param initial size of the table
- * @param steps for each capacity increase
- * @param function for hashing values
+ * @param capacity of the table
+ * @param hash function for keys
  * @return a new as_map
  */
-as_map * as_hashmap_new(uint32_t, uint32_t, as_val_hash_function);
+as_map * as_hashmap_new(uint32_t);
--- as_map.h
+++ as_map.h
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 
 typedef struct as_map_s as_map;
+typedef struct as_map_entry_s as_map_entry;
 typedef struct as_map_hooks_s as_map_hooks;
 
 
@@ -14,12 +15,18 @@ struct as_map_s {
     const as_map_hooks *    hooks;
 };
 
+struct as_map_entry_s {
+    as_val * key;
+    as_val * value;
+};
+
 struct as_map_hooks_s {
     int (*free)(as_map *);
+    uint32_t (*hash)(as_map *);
+
     uint32_t (* size)(const as_map *);
     int (* set)(as_map *, const as_val *, const as_val *);
     as_val * (* get)(const as_map *, const as_val *);
-    as_iterator * (* iterator)(const as_map *);
 };
 
 
@@ -29,15 +36,12 @@ as_map * as_map_new(void *, const as_map_hooks *);
 int as_map_free(as_map *);
 
 
-
-as_val * as_map_get(const as_map *, const as_val *);
+uint32_t as_map_size(const as_map *);
 
 int as_map_set(as_map *, const as_val *, const as_val *);
 
-
+as_val * as_map_get(const as_map *, const as_val *);
 
 as_val * as_map_toval(const as_map *);
 
 as_map * as_map_fromval(const as_val *);
-
-as_iterator * as_map_iterator(const as_map *);
