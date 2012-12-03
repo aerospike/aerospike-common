927fd28216cc9ce45c604d126f36a49e602a4498
Chris Stivers <chris@aerospike.com>
2012-12-03 11:23:11 -0800

    added as_serializer to Makefile

--- as_serializer.c
+++ as_serializer.c
@@ -5,7 +5,6 @@
  ******************************************************************************/
 
 extern inline int as_serializer_init(as_serializer *, const void *, const as_serializer_hooks *);
-extern inline as_serializer * as_serializer_new(const void *, const as_serializer_hooks *);
 extern inline void * as_serializer_source(as_serializer *);
 extern inline int as_serializer_free(as_serializer *);
 extern inline int as_serializer_serialize(as_serializer *, as_val *, as_buffer *);
