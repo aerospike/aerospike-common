9047e157dd6e76fdbf3983e9c7cd073a4479682d
Chris Stivers <chris@aerospike.com>
2012-12-04 11:26:27 -0800

    changes as_empty to as_nil.

---
+++ as_nil.c
@@ -0,0 +1,41 @@
+#include "as_val.h"
+#include <string.h>
+
+/******************************************************************************
+ * STATIC FUNCTIONS
+ ******************************************************************************/
+
+static int as_nil_val_free(as_val *);
+static uint32_t as_nil_val_hash(as_val *);
+static char * as_nil_val_tostring(as_val *);
+
+/******************************************************************************
+ * VARIABLES
+ ******************************************************************************/
+
+/**
+ * Represents empty values. As in a value with no value.
+ */
+extern const as_val as_nil = {
+    .type       = AS_NIL, 
+    .size       = 0,
+    .free       = as_nil_val_free,
+    .hash       = as_nil_val_hash,
+    .tostring   = as_nil_val_tostring
+};
+
+/******************************************************************************
+ * FUNCTIONS
+ ******************************************************************************/
+
+static int as_nil_val_free(as_val * v) {
+    return 0;
+}
+
+static uint32_t as_nil_val_hash(as_val * v) {
+    return 0;
+}
+
+static char * as_nil_val_tostring(as_val * v) {
+    return strdup("NIL");
+}
