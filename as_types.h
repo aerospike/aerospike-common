089cc81fad38e3a9eee3936724ed826bbd9caf8d
Chris Stivers <chris@aerospike.com>
2012-12-04 11:26:03 -0800

    added as_iterator, as_stream and as_result to as_types.h

--- as_types.h
+++ as_types.h
@@ -9,9 +9,12 @@
 #include "as_list.h"
 #include "as_map.h"
 #include "as_rec.h"
+#include "as_iterator.h"
+#include "as_stream.h"
 
 #include "as_arraylist.h"
 #include "as_linkedlist.h"
 
 #include "as_hashmap.h"
 
+#include "as_result.h"
