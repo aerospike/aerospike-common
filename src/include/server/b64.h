d2752f80dc4cbd760a840bf9ff95ab03d0027978
Chris Stivers <chris@aerospike.com>
2012-12-07 18:43:36 -0800

    fixed bad aliases

--- src/include/server/b64.h
+++ src/include/server/b64.h
@@ -6,10 +6,10 @@
  ******************************************************************************/
 
 // Move these to a CF file at some point if they work
-#define cf_base64_validate_input base64_validate_input
-#define cf_base64_encode_maxlen base64_encode_maxlen
-#define cf_base64_encode base64_encode
-#define cf_base64_tostring base64_tostring
-#define cf_base64_decode_inplace base64_decode_inplace
-#define cf_base64_decode base64_decode
-#define cf_base64_test base64_test
+#define base64_validate_input cf_base64_validate_input
+#define base64_encode_maxlen cf_base64_encode_maxlen
+#define base64_encode cf_base64_encode
+#define base64_tostring cf_base64_tostring
+#define base64_decode_inplace cf_base64_decode_inplace
+#define base64_decode cf_base64_decode
+#define base64_test cf_base64_test
