27b0836bb6ab173f7c8e55eeb0260f00ea79c80c
Chris Stivers <chris@aerospike.com>
2012-12-11 20:16:24 -0800

    improved as_map_val_tostring

--- as_map.c
+++ as_map.c
@@ -1,4 +1,5 @@
 #include "as_map.h"
+#include "as_pair.h"
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
@@ -65,8 +66,51 @@ static uint32_t as_map_val_hash(as_val * v) {
 
 static char * as_map_val_tostring(as_val * v) {
     if ( as_val_type(v) != AS_MAP ) return NULL;
-    char * str = (char *) malloc(sizeof(char) * 64);
-    bzero(str,64);
-    strcpy(str,"Map()");
-    return str;
+
+    as_map * m = (as_map *) v;
+
+    char *      buf = NULL;
+    uint32_t    blk = 256;
+    uint32_t    cap = blk;
+    uint32_t    pos = 0;
+    bool        sep = false;
+
+    buf = (char *) malloc(sizeof(char) * cap);
+    bzero(buf, sizeof(char) * cap);
+
+    strcpy(buf, "Map(");
+    pos += 4;
+    
+    as_iterator * i = as_map_iterator(m);
+    while ( as_iterator_has_next(i) ) {
+        as_pair * pair = (as_pair *) as_iterator_next(i);
+
+        char * keystr = as_val_tostring(as_pair_1(pair));
+        size_t keylen = strlen(keystr);
+
+        char * valstr = as_val_tostring(as_pair_2(pair));
+        size_t vallen = strlen(valstr);
+
+        if ( pos + keylen + 2 + vallen + 2 >= cap ) {
+            uint32_t adj = keylen+2+vallen+2 > blk ? keylen+2+vallen+2 : blk;
+            buf = realloc(buf, sizeof(char) * (cap + adj));
+            bzero(buf+cap, sizeof(char)*adj);
+            cap += adj;
+        }
+
+        if ( sep ) {
+            strcpy(buf + pos, ", ");
+            pos += 2;
+        }
+
+        strncpy(buf + pos, keystr, keylen);
+        strcpy(buf + pos + keylen, "->");
+        strncpy(buf + pos + keylen + 2, valstr, vallen);
+        pos += keylen + 2 + vallen;
+        sep = true;
+    }
+
+    strcpy(buf + pos, ")");
+    
+    return buf;
 }
