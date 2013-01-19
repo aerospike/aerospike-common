e733b7d9290e8800361f667dd254d1bb28f4f10b
Chris Stivers <chris@stivers.us>
2013-01-19 13:54:44 -0800

    cleaned up a bit

--- as_result.h
+++ as_result.h
@@ -19,13 +19,15 @@ struct as_result_s {
  ******************************************************************************/
 
 
-inline int as_result_init(as_result * r, bool is_success, as_val * v) {
+inline as_result * as_result_init(as_result * r, bool is_success, as_val * v) {
+    if ( !r ) return r;
     r->is_success = is_success;
     r->value = v;
-    return 0;
+    return r;
 }
 
 inline int as_result_destroy(as_result * r) {
+    if ( !r ) return 0;
     r->is_success = false;
     if ( r->value ) as_val_free(r->value);
     r->value = NULL;
@@ -34,25 +36,24 @@ inline int as_result_destroy(as_result * r) {
 
 inline int as_result_free(as_result * r) {
     as_result_destroy(r);
+    free(r);
     return 0;
 }
 
 inline as_result * as_success(as_val * v) {
     as_result * r = (as_result *) malloc(sizeof(as_result));
-    as_result_init(r, true, v);
-    return r;
+    return as_result_init(r, true, v);
 }
 
 inline as_result * as_failure(as_val * e) {
     as_result * r = (as_result *) malloc(sizeof(as_result));
-    as_result_init(r, false, e);
-    return r;
+    return as_result_init(r, false, e);
 }
 
-inline int as_result_tosuccess(as_result * r, as_val * v) {
+inline as_result * as_result_tosuccess(as_result * r, as_val * v) {
     return as_result_init(r, true, v);
 }
 
-inline int as_result_tofailure(as_result * r, as_val * e) {
+inline as_result * as_result_tofailure(as_result * r, as_val * e) {
     return as_result_init(r, false, e);
 }
