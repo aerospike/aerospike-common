c5551a12d2733a36eceb5195e339267aff61f388
Chris Stivers <chris@stivers.us>
2013-01-21 16:26:07 -0800

    fixed types

--- as_module.c
+++ as_module.c
@@ -7,5 +7,5 @@
 extern inline void * as_module_source(as_module *);
 extern inline int as_module_init(as_module *);
 extern inline int as_module_configure(as_module *, void *);
-extern inline int as_module_apply_record(as_module *, as_aerospike *, const char *, const char *, as_rec, as_list *, as_result *);
-extern inline int as_module_apply_stream(as_module *, as_aerospike *, const char *, const char *, as_stream, as_list *, as_result *);
+extern inline int as_module_apply_record(as_module *, as_aerospike *, const char *, const char *, as_rec *, as_list *, as_result *);
+extern inline int as_module_apply_stream(as_module *, as_aerospike *, const char *, const char *, as_stream *, as_list *, as_result *);
