458830d4951107648015d728eadaa1b97d4d70b1
Brian Bulkowski <brian@bulkowski.org>
2013-02-14 10:29:13 -0800

    New as_result semantics

--- as_result.h
+++ as_result.h
@@ -22,29 +22,23 @@ struct as_result_s {
  * FUNCTIONS
  ******************************************************************************/
 
-as_result * as_result_init(as_result *r, bool is_success, as_val *value);
-as_result * as_result_new(bool is_success, as_val *value);
+as_result * as_result_init(as_result *r);
+as_result * as_result_new();
 
 as_result * as_result_reserve(as_result *r);
 
 void as_result_destroy(as_result *);
 
-// These functions new an as_result object
-as_result * as_success(as_val *);
-as_result * as_failure(as_val *);
+// These functions new an as_result object - consume the as_val
+as_result * as_success_new(as_val *);
+as_result * as_failure_new(as_val *);
+
+// These functions init an as_result object - consume the as_val
+as_result * as_success_init(as_result *, as_val *);
+as_result * as_failure_init(as_result *, as_val *);
 
 // retrieves the value associated with success or failure
 as_val * as_result_value(as_result *);
 
-/******************************************************************************
- * INLINE FUNCTIONS
- ******************************************************************************/
-
-// These helper functions initialize an existing stack structure to success or failure
-inline as_result * as_result_tosuccess(as_result * r, as_val * v) {
-    return as_result_init(r, true, v);
-}
-
-inline as_result * as_result_tofailure(as_result * r, as_val * e) {
-    return as_result_init(r, false, e);
-}
+as_result * as_result_setsuccess(as_result * r, as_val * v);
+as_result * as_result_setfailure(as_result * r, as_val * v);
