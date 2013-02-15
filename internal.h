2069a45787090a5ad2ecae11de5411e5cc2c03bc
Chris Stivers <chris@stivers.us>
2013-02-14 21:04:06 -0800

    stream aggregation (partially working, kinda)

--- internal.h
+++ internal.h
@@ -5,6 +5,6 @@
 //
 
 #define LOG(fmt, args...) \
-    // __log_append(__FILE__, __LINE__, fmt, ## args);
+    __log_append(__FILE__, __LINE__, fmt, ## args);
 
 void __log_append(const char * file, int line, const char * fmt, ...);
