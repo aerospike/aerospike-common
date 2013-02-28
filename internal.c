cb10237acef1ee026f59c3bbef1e8d8a61e7bb2e
Chris Stivers <chris@stivers.us>
2013-02-27 16:14:45 -0800

    internal logging now logs larger messages

--- internal.c
+++ internal.c
@@ -4,10 +4,10 @@
 #include "internal.h"
 
 void __log_append(const char * file, int line, const char * fmt, ...) {
-    char msg[128] = {0};
+    char msg[1024] = {0};
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(msg, 128, fmt, ap);
+    vsnprintf(msg, 1024, fmt, ap);
     va_end(ap);
     printf("%s:%d – %s\n",file,line,msg);
 }
