ca9faa989d5d51cc1882d4102d91847dc0acc413
Brian Bulkowski <brian@bulkowski.org>
2013-03-08 10:22:49 -0800

    Use the particle bytes for the wire protocol so we can extend eventually this completes Bytes functionality for now

--- as_bytes.h
+++ as_bytes.h
@@ -6,6 +6,36 @@
 #include "as_util.h"
 #include "as_val.h"
 
+/********
+* CONSTANTS
+*********/
+
+// this section should use "proto.h" definitions
+// in aerospike-common but that header is not available
+// and is not yet consistant. Thus we will keep another copy
+// which must be in sync.
+
+typedef enum {
+    AS_BYTES_TYPE_NULL = 0,
+    AS_BYTES_TYPE_INTEGER = 1,
+    AS_BYTES_TYPE_FLOAT = 2,
+    AS_BYTES_TYPE_STRING = 3,
+    AS_BYTES_TYPE_BLOB = 4,
+    AS_BYTES_TYPE_TIMESTAMP = 5,
+    AS_BYTES_TYPE_DIGEST = 6,
+    AS_BYTES_TYPE_JAVA_BLOB = 7,
+	AS_BYTES_TYPE_CSHARP_BLOB = 8,
+	AS_BYTES_TYPE_PYTHON_BLOB = 9,
+	AS_BYTES_TYPE_RUBY_BLOB = 10,
+	AS_BYTES_TYPE_ERLANG_BLOB = 11,
+	AS_BYTES_TYPE_APPEND = 12,
+	AS_BYTES_TYPE_LUA_BLOB = 13,
+	AS_BYTES_TYPE_JSON_BLOB = 14,
+    AS_BYTES_TYPE_MAP = 15,
+    AS_BYTES_TYPE_LIST = 16,
+    AS_BYTES_TYPE_MAX = 17
+} as_bytes_type;
+
 /******************************************************************************
  * TYPES
  ******************************************************************************/
@@ -15,6 +45,7 @@ typedef struct as_bytes_s as_bytes;
 struct as_bytes_s {
     as_val _;
     bool value_is_malloc; 
+    as_bytes_type  type;
     uint8_t * value;
     size_t len; // length of bytes allocated in value == size
     size_t capacity;
@@ -46,6 +77,10 @@ inline uint8_t * as_bytes_tobytes(const as_bytes * s) {
 size_t as_bytes_len(as_bytes * s);
 
 uint32_t as_bytes_hash(const as_bytes * s);
+
+as_bytes_type as_bytes_get_type(const as_bytes * s);
+
+void as_bytes_set_type(as_bytes *s, as_bytes_type t);
 
 // copy from as_bytes to a buf
 int as_bytes_get(const as_bytes * s, int index, uint8_t *buf, int buf_len);
