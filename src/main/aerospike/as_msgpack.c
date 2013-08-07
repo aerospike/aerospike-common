69b88d32cb84419f560841d129a7570c218e8bcd
Chris Stivers <chris@stivers.us>
2013-08-07 16:37:36 -0700

    cleaned up msgpack debugging code

--- src/main/aerospike/as_msgpack.c
+++ src/main/aerospike/as_msgpack.c
@@ -53,23 +53,69 @@ static int as_msgpack_map_to_val(msgpack_object_map *, as_val **);
  * FUNCTIONS
  ******************************************************************************/
 
-int as_msgpack_pack_val(msgpack_packer * pk, as_val * v) {
-	if ( v == NULL ) return 1;
-	switch( as_val_type(v) ) {
-		case AS_NIL		: return as_msgpack_pack_nil(pk);
-		case AS_BOOLEAN : return as_msgpack_pack_boolean(pk, (as_boolean *) v);
-		case AS_INTEGER : return as_msgpack_pack_integer(pk, (as_integer *) v);
-		case AS_STRING  : return as_msgpack_pack_string(pk, (as_string *) v);
-		case AS_BYTES   : return as_msgpack_pack_bytes(pk, (as_bytes *) v);
-		case AS_LIST    : return as_msgpack_pack_list(pk, (as_list *) v);
-		case AS_MAP     : return as_msgpack_pack_map(pk, (as_map *) v);
-		case AS_REC     : return as_msgpack_pack_rec(pk, (as_rec *) v);
-		case AS_PAIR    : return as_msgpack_pack_pair(pk, (as_pair *) v);
-		default         : return 2;
+int as_msgpack_pack_val(msgpack_packer * pk, as_val * val)
+{
+#if LOG_ENABLED==1
+	char * v = as_val_tostring(val);
+	LOG("as_msgpack_pack_val : start : %s",v);
+	free(v);
+#endif
+
+	int rc = 0;
+
+	if ( val == NULL ) {
+		LOG("as_msgpack_pack_val : value is null");
+		rc = 1;
 	}
+	else {	
+		switch( as_val_type(val) ) {
+			case AS_NIL : 
+				rc = as_msgpack_pack_nil(pk);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_nil : %d", rc);
+				break;
+			case AS_BOOLEAN : 
+				rc = as_msgpack_pack_boolean(pk, (as_boolean *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_boolean : %d", rc);
+				break;
+			case AS_INTEGER : 
+				rc = as_msgpack_pack_integer(pk, (as_integer *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_integer : %d", rc);
+				break;
+			case AS_STRING : 
+				rc = as_msgpack_pack_string(pk, (as_string *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_string : %d", rc);
+				break;
+			case AS_BYTES : 
+				rc = as_msgpack_pack_bytes(pk, (as_bytes *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_bytes : %d", rc);
+				break;
+			case AS_LIST : 
+				rc = as_msgpack_pack_list(pk, (as_list *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_list : %d", rc);
+				break;
+			case AS_MAP : 
+				rc = as_msgpack_pack_map(pk, (as_map *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_map : %d", rc);
+				break;
+			case AS_REC : 
+				rc = as_msgpack_pack_rec(pk, (as_rec *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_rec : %d", rc);
+				break;
+			case AS_PAIR : 
+				rc = as_msgpack_pack_pair(pk, (as_pair *) val);
+				LOG_COND(rc, "as_msgpack_pack_val : as_msgpack_pack_pair : %d", rc);
+				break;
+			default : 
+				rc = 2;
+		}
+	}
+
+	LOG("as_msgpack_pack_val : end : %d", rc);
+	return rc;
 }
 
-int as_msgpack_object_to_val(msgpack_object * object, as_val ** val) {
+int as_msgpack_object_to_val(msgpack_object * object, as_val ** val) 
+{
 	if ( object == NULL ) return 1;
 	switch( object->type ) {
 		case MSGPACK_OBJECT_NIL             	: return as_msgpack_nil_to_val(val);
@@ -91,10 +137,8 @@ static int as_msgpack_pack_nil(msgpack_packer * pk)
 {
 	LOG("as_msgpack_pack_nil : start");
 	int rc = msgpack_pack_nil(pk);
-	if ( rc ) {
-		LOG("as_msgpack_pack_nil : msgpack_pack_nil : %d",rc);
-	}
-	LOG("as_msgpack_pack_nil : %d", rc);
+	LOG_COND(rc, "as_msgpack_pack_nil : msgpack_pack_nil : %d",rc);
+	LOG("as_msgpack_pack_nil : end : %d", rc);
 	return rc;
 }
 
@@ -105,18 +149,14 @@ static int as_msgpack_pack_boolean(msgpack_packer * pk, as_boolean * b)
 
 	if ( as_boolean_get(b) == true ) {
 		rc = msgpack_pack_true(pk);
-		if ( rc ) {
-			LOG("as_msgpack_pack_boolean : msgpack_pack_true : %d",rc);
-		}
+		LOG_COND(rc, "as_msgpack_pack_boolean : msgpack_pack_true : %d",rc);
 	}
 	else {
 		rc = msgpack_pack_false(pk);
-		if ( rc ) {
-			LOG("as_msgpack_pack_boolean : msgpack_pack_false : %d",rc);
-		}
+		LOG_COND(rc, "as_msgpack_pack_boolean : msgpack_pack_false : %d",rc);
 	}
 
-	LOG("as_msgpack_pack_boolean : %d", rc);
+	LOG("as_msgpack_pack_boolean : end : %d", rc);
 	return rc;
 }
 
@@ -126,11 +166,9 @@ static int as_msgpack_pack_integer(msgpack_packer * pk, as_integer * i)
 	int rc = 0;
 
 	rc = msgpack_pack_int64(pk, as_integer_get(i));
-	if ( rc ) {
-		LOG("as_msgpack_pack_integer : msgpack_pack_int64 : %d",rc);
-	}
+	LOG_COND(rc, "as_msgpack_pack_integer : msgpack_pack_int64 : %d",rc);
 
-	LOG("as_msgpack_pack_integer : %d", rc);
+	LOG("as_msgpack_pack_integer : end : %d", rc);
 	return rc;
 }
 
@@ -146,17 +184,13 @@ static int as_msgpack_pack_string(msgpack_packer * pk, as_string * s)
 	memcpy(raw + 1, s->value, s->len);
 
 	rc = msgpack_pack_raw(pk, len);
-	if ( rc ) {
-		LOG("as_msgpack_pack_string : msgpack_pack_raw : %d",rc);
-	}
-	else {
+	LOG_COND(rc, "as_msgpack_pack_string : msgpack_pack_raw : %d",rc);
+	if ( rc == 0 ) {
 		rc = msgpack_pack_raw_body(pk, raw, len);
-		if ( rc ) {
-			LOG("as_msgpack_pack_string : msgpack_pack_raw_body : %d",rc);
-		}
+		LOG_COND(rc, "as_msgpack_pack_string : msgpack_pack_raw_body : %d",rc);
 	}
 
-	LOG("as_msgpack_pack_string : %d", rc);
+	LOG("as_msgpack_pack_string : end : %d", rc);
 	return rc;
 }
 
@@ -171,17 +205,13 @@ static int as_msgpack_pack_bytes(msgpack_packer * pk, as_bytes * b)
 	memcpy(raw + 1, b->value, b->size);
 
 	rc = msgpack_pack_raw(pk, len);
-	if ( rc ) {
-		LOG("as_msgpack_pack_bytes : msgpack_pack_raw : %d",rc);
-	}
-	else {	
+	LOG_COND(rc, "as_msgpack_pack_bytes : msgpack_pack_raw : %d",rc);
+	if ( rc == 0 ) {	
 		rc = msgpack_pack_raw_body(pk, raw, len);
-		if ( rc ) {
-			LOG("as_msgpack_pack_bytes : msgpack_pack_raw_body : %d",rc);
-		}
+		LOG_COND(rc, "as_msgpack_pack_bytes : msgpack_pack_raw_body : %d",rc);
 	}
 
-	LOG("as_msgpack_pack_bytes : %d", rc);
+	LOG("as_msgpack_pack_bytes : end : %d", rc);
 	return rc;
 }
 
@@ -193,9 +223,7 @@ static bool as_msgpack_pack_list_foreach(as_val * val, void * udata)
 	msgpack_packer * pk = (msgpack_packer *) udata;
 
 	rc = as_msgpack_pack_val(pk, val);
-	if ( rc ) {
-		LOG("as_msgpack_pack_list_foreach : as_msgpack_pack_val : %d",rc);
-	}
+	LOG_COND(rc, "as_msgpack_pack_list_foreach : as_msgpack_pack_val : %d",rc);
 
 	LOG("as_msgpack_pack_list_foreach : %d", rc);
 	return rc == 0;
@@ -207,38 +235,41 @@ static int as_msgpack_pack_list(msgpack_packer * pk, as_list * l)
 	int rc = 0;
 
 	rc = msgpack_pack_array(pk, as_list_size(l));
-	if ( rc ) {
-		LOG("as_msgpack_pack_list : msgpack_pack_array > %d",rc);
-	}
-	else {
+	LOG_COND(rc, "as_msgpack_pack_list : msgpack_pack_array : %d",rc);
+
+	if ( rc == 0 ) {
 		rc = as_list_foreach(l, as_msgpack_pack_list_foreach, pk) == true ? 0 : 1;
-		LOG("as_msgpack_pack_map : as_map_foreach : %d", rc);
+		LOG_COND(rc, "as_msgpack_pack_map : as_map_foreach : %d", rc);
 	}
 
-	LOG("as_msgpack_pack_list : %d",rc);
+	LOG("as_msgpack_pack_list : end : %d",rc);
 	return rc;
 }
 
 static bool as_msgpack_pack_map_foreach(const as_val * key, const as_val * val, void * udata)
 {
-	LOG("as_msgpack_pack_map_foreach : start");
+#if LOG_ENABLED==1
+	char * k = as_val_tostring(key);
+	char * v = as_val_tostring(val);
+	LOG("as_msgpack_pack_map_foreach : start : %s=%s",k,v);
+	free(k);
+	free(v);
+#endif
+
 	int rc = 0;
 
 	msgpack_packer * pk = (msgpack_packer *) udata;
 
 	rc = as_msgpack_pack_val(pk, (as_val *) key);
-	if ( rc ) {
-		LOG("as_msgpack_pack_map_foreach : as_msgpack_pack_val : %d",rc);
-	}
-	else {	
+	LOG_COND(rc, "as_msgpack_pack_map_foreach : as_msgpack_pack_val : %d",rc);
+
+	if ( rc == 0 ) {	
 		rc = as_msgpack_pack_val(pk, (as_val *) val);
-		if ( rc ) {
-			LOG("as_msgpack_pack_map_foreach : as_msgpack_pack_val : %d",rc);
-		}
+		LOG_COND(rc, "as_msgpack_pack_map_foreach : as_msgpack_pack_val : %d",rc);
 	}
 
-	LOG("as_msgpack_pack_map_foreach : %d", rc);
-	return rc == 0;
+	LOG("as_msgpack_pack_map_foreach : end : %d", rc);
+	return true;//rc == 0;
 }
 
 static int as_msgpack_pack_map(msgpack_packer * pk, as_map * m)
@@ -247,16 +278,13 @@ static int as_msgpack_pack_map(msgpack_packer * pk, as_map * m)
 	int rc = 0;
 
 	rc = msgpack_pack_map(pk, as_map_size(m));
-	if ( rc ) {
-		LOG("as_msgpack_pack_map : msgpack_pack_map : %d",rc);
-		return rc;
-	}
-	else {
+	LOG_COND(rc, "as_msgpack_pack_map : msgpack_pack_map : %d",rc);
+	if ( rc == 0 ) {
 		rc = as_map_foreach(m, as_msgpack_pack_map_foreach, pk) == true ? 0 : 1;
-		LOG("as_msgpack_pack_map : as_map_foreach : %d", rc);
+		LOG_COND(rc, "as_msgpack_pack_map : as_map_foreach : %d", rc);
 	}
 
-	LOG("as_msgpack_pack_map : %d", rc);
+	LOG("as_msgpack_pack_map : end : %d", rc);
 	return rc;
 }
 
@@ -272,23 +300,19 @@ static int as_msgpack_pack_pair(msgpack_packer * pk, as_pair * p)
 	int rc = 0;
 
 	rc = msgpack_pack_array(pk, 2);
-	if ( rc ) {
-		LOG("as_msgpack_pack_pair : msgpack_pack_array : %d",rc);
-	}
-	else {
+	LOG_COND(rc, "as_msgpack_pack_pair : msgpack_pack_array : %d",rc);
+
+	if ( rc == 0 ) {
 		rc = as_msgpack_pack_val(pk, as_pair_1(p));
-		if ( rc ) {
-			LOG("as_msgpack_pack_pair : as_msgpack_pack_val : %d",rc);
-		}
-		else {
+		LOG_COND(rc, "as_msgpack_pack_pair : as_msgpack_pack_val : %d",rc);
+
+		if ( rc == 0 ) {
 			rc = as_msgpack_pack_val(pk, as_pair_2(p));
-			if ( rc ) {
-				LOG("as_msgpack_pack_pair : as_msgpack_pack_val : %d",rc);
-			}
+			LOG_COND(rc, "as_msgpack_pack_pair : as_msgpack_pack_val : %d",rc);
 		}
 	}
 
-	LOG("as_msgpack_pack_pair : %d", rc);
+	LOG("as_msgpack_pack_pair : end : %d", rc);
 	return rc;
 }
 
