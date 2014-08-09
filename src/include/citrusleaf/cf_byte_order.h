010707f899aaf5c90a1a4bc16ea3790af82efd02
Brian Nichols <bnichols@aerospike.com>
2014-08-08 19:39:56 -0700

    Add little endian swap methods.

--- src/include/citrusleaf/cf_byte_order.h
+++ src/include/citrusleaf/cf_byte_order.h
@@ -22,13 +22,19 @@
 #include <asm/byteorder.h>
 
 #define cf_swap_to_be16(_n) __cpu_to_be16(_n)
+#define cf_swap_to_le16(_n) __cpu_to_le16(_n)
 #define cf_swap_from_be16(_n) __be16_to_cpu(_n)
+#define cf_swap_from_le16(_n) __le16_to_cpu(_n)
 
 #define cf_swap_to_be32(_n) __cpu_to_be32(_n)
+#define cf_swap_to_le32(_n) __cpu_to_le32(_n)
 #define cf_swap_from_be32(_n) __be32_to_cpu(_n)
+#define cf_swap_from_le32(_n) __le32_to_cpu(_n)
 
 #define cf_swap_to_be64(_n) __cpu_to_be64(_n)
+#define cf_swap_to_le64(_n) __cpu_to_le64(_n)
 #define cf_swap_from_be64(_n) __be64_to_cpu(_n)
+#define cf_swap_from_le64(_n) __le64_to_cpu(_n)
 
 #endif // __linux__
 
@@ -37,13 +43,19 @@
 #include <arpa/inet.h>
 
 #define cf_swap_to_be16(_n) OSSwapHostToBigInt16(_n)
+#define cf_swap_to_le16(_n) OSSwapHostToLittleInt16(_n)
 #define cf_swap_from_be16(_n) OSSwapBigToHostInt16(_n)
+#define cf_swap_from_le16(_n) OSSwapLittleToHostInt16(_n)
 
 #define cf_swap_to_be32(_n) OSSwapHostToBigInt32(_n)
+#define cf_swap_to_le32(_n) OSSwapHostToLittleInt32(_n)
 #define cf_swap_from_be32(_n) OSSwapBigToHostInt32(_n)
+#define cf_swap_from_le32(_n) OSSwapLittleToHostInt32(_n)
 
 #define cf_swap_to_be64(_n) OSSwapHostToBigInt64(_n)
+#define cf_swap_to_le64(_n) OSSwapHostToLittleInt64(_n)
 #define cf_swap_from_be64(_n) OSSwapBigToHostInt64(_n)
+#define cf_swap_from_le64(_n) OSSwapLittleToHostInt64(_n)
 
 #endif // __APPLE__
 
@@ -53,11 +65,17 @@
 #include <WinSock2.h>
 
 #define cf_swap_to_be16(_n) _byteswap_uint16(_n)
+#define cf_swap_to_le16(_n) (_n)
 #define cf_swap_from_be16(_n) _byteswap_uint16(_n)
+#define cf_swap_from_le16(_n) (_n)
 
 #define cf_swap_to_be32(_n) _byteswap_uint32(_n)
+#define cf_swap_to_le32(_n) (_n)
 #define cf_swap_from_be32(_n) _byteswap_uint32(_n)
+#define cf_swap_from_le32(_n) (_n)
 
 #define cf_swap_to_be64(_n) _byteswap_uint64(_n)
+#define cf_swap_to_le64(_n) (_n)
 #define cf_swap_from_be64(_n) _byteswap_uint64(_n)
+#define cf_swap_from_le64(_n) (_n)
 #endif // CF_WINDOWS
