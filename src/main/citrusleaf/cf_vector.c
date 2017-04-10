f54668df97428f755db66f004492fd15167ced78
Shu Chen <shu@aerospike.com>
2017-04-10 15:17:53 -0700

    Fix deadlocks on many vector failures. Style consistency changes.

--- src/main/citrusleaf/cf_vector.c
+++ src/main/citrusleaf/cf_vector.c
@@ -15,6 +15,10 @@
  * the License.
  */
 
+//==========================================================
+// Includes.
+//
+
 #include <string.h>
 #include <stdlib.h>
 
@@ -22,301 +26,404 @@
 
 #include <citrusleaf/alloc.h>
 
-/******************************************************************************
- * MACROS
- ******************************************************************************/
 
-#define VECTOR_LOCK(_v) 	pthread_mutex_lock(&((cf_vector *)_v)->LOCK)
-#define VECTOR_UNLOCK(_v) 	pthread_mutex_unlock(&((cf_vector *)_v)->LOCK)
+//==========================================================
+// Constants & typedefs.
+//
 
-/******************************************************************************
- * FUNCTIONS
- ******************************************************************************/
+#define VECTOR_FLAG_FREE_SELF   0x10
+#define VECTOR_FLAG_FREE_VECTOR 0x20
 
-cf_vector *cf_vector_create(uint32_t value_len, uint32_t init_sz, uint flags) {
-	cf_vector *v;
 
-	v = cf_malloc(sizeof(cf_vector));
-	if (!v)	return(0);
+//==========================================================
+// Forward declarations.
+//
 
-	v->value_len = value_len;
-	v->flags = flags;
-	v->alloc_len = init_sz;
-	v->len = 0;
-	v->stack_struct = false;
-	v->stack_vector = false;
-	if (init_sz) {
-		v->vector = cf_malloc(init_sz * value_len);
-		if (!v->vector)	{
-			cf_free(v);
-			return(0);
-		}
-	}
-	else
-		v->vector = 0;
-	if ((flags & VECTOR_FLAG_INITZERO) && v->vector)
-		memset(v->vector, 0, init_sz * value_len);
-	if (flags & VECTOR_FLAG_BIGLOCK){
-		pthread_mutex_init(&v->LOCK, 0);
-	}
-	return(v);
+static bool vector_resize(cf_vector *v, uint32_t new_count);
+
+
+//==========================================================
+// Macros.
+//
+
+#define VECTOR_LOCK(_v) if (v->flags & VECTOR_FLAG_BIGLOCK) { \
+		pthread_mutex_lock(&((cf_vector *)_v)->LOCK); \
 }
 
-int cf_vector_init(cf_vector *v, uint32_t value_len, uint32_t init_sz, uint flags) {
-	v->value_len = value_len;
-	v->flags = flags;
-	v->alloc_len = init_sz;
-	v->len = 0;
-	v->stack_struct = true;
-	v->stack_vector = false;
-	if (init_sz) {
-		v->vector = cf_malloc(init_sz * value_len);
-		if (!v->vector)	return(-1);
-	}
-	else
-		v->vector = 0;
-	if ((flags & VECTOR_FLAG_INITZERO) && v->vector)
-		memset(v->vector, 0, init_sz * value_len);
-	if (flags & VECTOR_FLAG_BIGLOCK){
-		pthread_mutex_init(&v->LOCK, 0);
-	}
-	return(0);
-}
-
-void cf_vector_init_smalloc(cf_vector *v, uint32_t value_len, uint8_t *sbuf, int sbuf_sz, uint flags) {
-	v->value_len = value_len;
-	v->flags = flags;
-	v->alloc_len = sbuf_sz / value_len;
-	v->len = 0;
-	v->stack_struct = true;
-	v->stack_vector = true;
-	v->vector = sbuf;
-	if ((flags & VECTOR_FLAG_INITZERO) && v->vector)
-		memset(v->vector, 0, sbuf_sz);
-	if (flags & VECTOR_FLAG_BIGLOCK){
-		pthread_mutex_init(&v->LOCK, 0);
-	}
-}
-
-void cf_vector_clone_stack(cf_vector *v, cf_vector *target, uint8_t *target_buf) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-
-	target->value_len = v->value_len;
-	target->flags = v->flags;
-	target->alloc_len = v->len;
-	target->len = v->len;
-	target->stack_struct = true;
-	target->stack_vector = true;
-	target->vector = target_buf;
-	memcpy(target->vector, v->vector, v->value_len * v->len);
-
-	if (target->flags & VECTOR_FLAG_BIGLOCK){
-		pthread_mutex_init(&target->LOCK, 0);
-	}
-
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-}
-
-void cf_vector_destroy(cf_vector *v) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK){
-		pthread_mutex_destroy(&v->LOCK);
-	}
-	if (v->vector && (v->stack_vector == false))	cf_free(v->vector);
-	if (v->stack_struct == false) cf_free(v);
-}
-
-static int cf_vector_resize(cf_vector *v, uint32_t new_sz) {
-	if (v->flags & VECTOR_FLAG_BIGRESIZE) {
-		if (new_sz < 50)	new_sz = 50;
-	}
-	else if (new_sz == 0) {
-		new_sz = 2;
-	}
-	uint8_t *_t;
-	if (v->vector == 0 || v->stack_vector) {
-		_t = cf_malloc(new_sz * v->value_len);
-		if (!_t)	return(-1);
-		if (v->stack_vector) {
-			memcpy(_t, v->vector, v->alloc_len * v->value_len);
-			v->stack_vector = false;
-		}
-	}
-	else
-		_t = cf_realloc(v->vector, (new_sz) * v->value_len);
-	if (!_t)	return(-1);
-	v->vector = _t;
-	if (v->flags & VECTOR_FLAG_INITZERO)
-		memset(v->vector + (v->alloc_len * v->value_len), 0, (new_sz - v->alloc_len) * v->value_len);
-	v->alloc_len = new_sz;
-	return(0);
-}
-
-int cf_vector_set(cf_vector *v, uint32_t index, const void *value) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-
-	// Return error if index is not within current array bounds.
-	if (index >= v->alloc_len)
-		return(-1);
-
-	memcpy(v->vector + (index * v->value_len), value, v->value_len);
-	if (index >= v->len)	v->len = index + 1;
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return(0);
-}
-
-int cf_vector_append_lockfree(cf_vector *v, const void *value) {
-	if (v->len >= v->alloc_len)
-		if (0 != cf_vector_resize(v, v->len * 2))	return(-1);
-	memcpy(v->vector + (v->len * v->value_len), value, v->value_len);
-	v->len ++;
-	return(0);
-
+#define VECTOR_UNLOCK(_v) if (v->flags & VECTOR_FLAG_BIGLOCK) { \
+		pthread_mutex_unlock(&((cf_vector *)_v)->LOCK); \
 }
 
 
+//==========================================================
+// Public API.
+//
 
-int cf_vector_append(cf_vector *v, const void *value) {
-	int rv;
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	rv = cf_vector_append_lockfree(v, value);
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return(rv);
-}
-
-int cf_vector_append_unique(cf_vector *v, const void *value) {
-	int rv=0;
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	uint8_t	*_b = v->vector;
-	uint32_t	_l = v->value_len;
-	for (uint i=0;i<v->len;i++) {
-		if (0 == memcmp(value, _b, _l)) {
-			goto Found;
-		}
-		_b += _l;
-	}
-	rv = cf_vector_append_lockfree(v, value);
-Found:
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return(rv);
-}
-
-int cf_vector_pop(cf_vector *v, void *value)
+cf_vector *
+cf_vector_create(uint32_t ele_sz, uint32_t count, uint32_t flags)
 {
-	if (v->flags & VECTOR_FLAG_BIGLOCK) {
-		VECTOR_LOCK(v);
+	cf_vector *v = cf_malloc(sizeof(cf_vector));
+
+	if (! v) {
+		return NULL;
 	}
 
-	int rv = 0;
+	if (cf_vector_init(v, ele_sz, count,
+			flags | VECTOR_FLAG_FREE_SELF) != 0) {
+		cf_free(v);
+		return NULL;
+	}
 
-	if (v->len != 0) {
-		v->len--;
-		memcpy(value, v->vector + v->len * v->value_len, v->value_len);
+	return v;
+}
+
+int
+cf_vector_init(cf_vector *v, uint32_t ele_sz, uint32_t count, uint32_t flags)
+{
+	uint8_t *buf;
+
+	if (count) {
+		if (! (buf = cf_malloc(count * ele_sz))) {
+			return -1;
+		}
 	}
 	else {
-		rv = -1;
+		buf = 0;
 	}
 
-	if (v->flags & VECTOR_FLAG_BIGLOCK) {
-		VECTOR_UNLOCK(v);
+	cf_vector_init_with_buf(v, ele_sz, count, buf,
+			flags | VECTOR_FLAG_FREE_VECTOR);
+
+	return 0;
+}
+
+void
+cf_vector_init_with_buf(cf_vector *v, uint32_t ele_sz, uint32_t count,
+		uint8_t *buf, uint32_t flags)
+{
+	v->ele_sz = ele_sz;
+	v->flags = flags;
+	v->alloc_cnt = count;
+	v->count = 0;
+	v->vector = buf;
+
+	if ((flags & VECTOR_FLAG_INITZERO) && v->vector) {
+		memset(v->vector, 0, count * ele_sz);
 	}
+
+	if (flags & VECTOR_FLAG_BIGLOCK) {
+		pthread_mutex_init(&v->LOCK, 0);
+	}
+}
+
+// Deprecated - use cf_vector_init_with_buf().
+void
+cf_vector_init_smalloc(cf_vector *v, uint32_t ele_sz, uint8_t *sbuf,
+		uint32_t sbuf_sz, uint32_t flags)
+{
+	cf_vector_init_with_buf(v, ele_sz, sbuf_sz / ele_sz, sbuf, flags);
+}
+
+void
+cf_vector_destroy(cf_vector *v)
+{
+	if (v->flags & VECTOR_FLAG_BIGLOCK) {
+		pthread_mutex_destroy(&v->LOCK);
+	}
+
+	if (v->vector && (v->flags & VECTOR_FLAG_FREE_VECTOR)) {
+		cf_free(v->vector);
+	}
+
+	if (v->flags & VECTOR_FLAG_FREE_SELF) {
+		cf_free(v);
+	}
+}
+
+int
+cf_vector_set(cf_vector *v, uint32_t idx, const void *val)
+{
+	VECTOR_LOCK(v);
+
+	if (idx >= v->alloc_cnt) {
+		VECTOR_UNLOCK(v);
+		return -1;
+	}
+
+	memcpy(v->vector + (idx * v->ele_sz), val, v->ele_sz);
+
+	if (idx >= v->count) {
+		v->count = idx + 1;
+	}
+
+	VECTOR_UNLOCK(v);
+
+	return 0;
+}
+
+int
+cf_vector_append_lockfree(cf_vector *v, const void *val)
+{
+	if (v->count >= v->alloc_cnt && ! vector_resize(v, v->count * 2)) {
+		return -1;
+	}
+
+	memcpy(v->vector + (v->count * v->ele_sz), val, v->ele_sz);
+	v->count++;
+
+	return 0;
+}
+
+int
+cf_vector_append(cf_vector *v, const void *val)
+{
+	VECTOR_LOCK(v);
+
+	int ret = cf_vector_append_lockfree(v, val);
+
+	VECTOR_UNLOCK(v);
+
+	return ret;
+}
+
+int
+cf_vector_append_unique(cf_vector *v, const void *val)
+{
+	VECTOR_LOCK(v);
+
+	uint8_t	*p = v->vector;
+	uint32_t ele_sz = v->ele_sz;
+
+	for (uint32_t i = 0; i < v->count; i++) {
+		if (memcmp(val, p, ele_sz) == 0) {
+			VECTOR_UNLOCK(v);
+			return 0;
+		}
+
+		p += ele_sz;
+	}
+
+	int rv = cf_vector_append_lockfree(v, val);
+
+	VECTOR_UNLOCK(v);
 
 	return rv;
 }
 
-// Copy the vector element into the pointer I give
-
-int cf_vector_get(const cf_vector *v, uint32_t index, void *value_p) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	if (index >= v->alloc_len)
-		return(-1);
-	memcpy(value_p, v->vector + (index * v->value_len), v->value_len);
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return(0);
-}
-
-void *cf_vector_getp(const cf_vector *v, uint32_t index) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	if (index >= v->alloc_len)
-		return(0);
-	void *r = v->vector + (index * v->value_len);
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return( r );
-}
-
-void *cf_vector_getp_vlock(const cf_vector *v, uint32_t index, pthread_mutex_t **vlock) {
-	if (!v->flags & VECTOR_FLAG_BIGLOCK)
-		return(0);
-	if (index >= v->alloc_len)
-		return(0);
+int
+cf_vector_pop(cf_vector *v, void *val)
+{
 	VECTOR_LOCK(v);
+
+	if (v->count == 0) {
+		VECTOR_UNLOCK(v);
+		return -1;
+	}
+
+	v->count--;
+	memcpy(val, v->vector + v->count * v->ele_sz, v->ele_sz);
+
+	VECTOR_UNLOCK(v);
+
+	return 0;
+}
+
+int
+cf_vector_get(const cf_vector *v, uint32_t idx, void *val)
+{
+	VECTOR_LOCK(v);
+
+	if (idx >= v->alloc_cnt) {
+		VECTOR_UNLOCK(v);
+		return -1;
+	}
+
+	memcpy(val, v->vector + (idx * v->ele_sz), v->ele_sz);
+
+	VECTOR_UNLOCK(v);
+
+	return 0;
+}
+
+bool
+cf_vector_get_sized(const cf_vector *v, uint32_t idx, void *val, uint32_t sz)
+{
+	if (v->ele_sz <= sz) {
+		return cf_vector_get(v, idx, val) == 0;
+	}
+
+	uint8_t buf[v->ele_sz];
+
+	if (cf_vector_get(v, idx, &buf) != 0) {
+		return false;
+	}
+
+	return true;
+}
+
+void *
+cf_vector_getp(const cf_vector *v, uint32_t idx)
+{
+	VECTOR_LOCK(v);
+
+	if (idx >= v->alloc_cnt) {
+		VECTOR_UNLOCK(v);
+		return NULL;
+	}
+
+	void *ele = v->vector + (idx * v->ele_sz);
+
+	VECTOR_UNLOCK(v);
+
+	return ele;
+}
+
+void *
+cf_vector_getp_vlock(const cf_vector *v, uint32_t idx, pthread_mutex_t **vlock)
+{
+	if (! v->flags & VECTOR_FLAG_BIGLOCK) {
+		return NULL;
+	}
+
+	VECTOR_LOCK(v);
+
+	if (idx >= v->alloc_cnt) {
+		VECTOR_UNLOCK(v);
+		return NULL;
+	}
+
 	*vlock = &((cf_vector *)v)->LOCK;
-	return(v->vector + (index * v->value_len));
+
+	return v->vector + (idx * v->ele_sz);
 }
 
-int cf_vector_delete(cf_vector *v, uint32_t index) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	// check bounds
-	if (index >= v->len)
-		return (-1);
-	// check for last - no copy
-	if (index != v->len - 1) {
-		memmove(v->vector + (index * v->value_len),
-				v->vector + ((index+1) * v->value_len),
-				(v->len - (index+1)) * v->value_len );
-	}
-	v->len --;
+int
+cf_vector_delete(cf_vector *v, uint32_t idx)
+{
+	VECTOR_LOCK(v);
 
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
+	if (idx >= v->count) {
 		VECTOR_UNLOCK(v);
-	return(0);
+		return -1;
+	}
+
+	if (idx != v->count - 1) {
+		memmove(v->vector + (idx * v->ele_sz),
+				v->vector + ((idx + 1) * v->ele_sz),
+				(v->count - (idx + 1)) * v->ele_sz );
+	}
+
+	v->count--;
+
+	VECTOR_UNLOCK(v);
+
+	return 0;
 }
 
-int cf_vector_delete_range(cf_vector *v, uint32_t idx_start, uint32_t idx_end) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	// check bounds
-	if (idx_start >= idx_end)
-		return (-1);
-	if (idx_start >= v->len)
-		return(-1);
-	if (idx_end > v->len)
-		return(-1);
+int
+cf_vector_delete_range(cf_vector *v, uint32_t start, uint32_t end)
+{
 
-	// Copy down if not at end
-	if (idx_end != v->len) {
-		memmove( v->vector + (idx_start * v->value_len),
-				v->vector + ((idx_end) * v->value_len),
-			    (v->len - idx_end) * v->value_len );
+	if (start >= end) {
+		return -1;
+	}
+
+	VECTOR_LOCK(v);
+
+	if (start >= v->count || end > v->count) {
+		VECTOR_UNLOCK(v);
+		return -1;
+	}
+
+	// Copy down if not at end.
+	if (end != v->count) {
+		memmove( v->vector + (start * v->ele_sz),
+				v->vector + ((end) * v->ele_sz),
+			    (v->count - end) * v->ele_sz );
 
 	}
-	v->len -= idx_end - idx_start;
 
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return(0);
+	v->count -= end - start;
+
+	VECTOR_UNLOCK(v);
+
+	return 0;
 }
 
-void cf_vector_compact(cf_vector *v) {
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_LOCK(v);
-	if (v->alloc_len && (v->len != v->alloc_len)) {
-		v->vector = cf_realloc(v->vector, v->len * v->alloc_len);
-		v->alloc_len = v->len;
+void
+cf_vector_clear(cf_vector *v)
+{
+	VECTOR_LOCK(v);
+
+	v->count = 0;
+
+	if (v->flags & VECTOR_FLAG_INITZERO) {
+		memset(v->vector, 0, v->alloc_cnt * v->ele_sz);
 	}
-	if (v->flags & VECTOR_FLAG_BIGLOCK)
-		VECTOR_UNLOCK(v);
-	return;
+
+	VECTOR_UNLOCK(v);
+}
+
+void
+cf_vector_compact(cf_vector *v)
+{
+	VECTOR_LOCK(v);
+
+	if (v->alloc_cnt && (v->count != v->alloc_cnt)) {
+		v->vector = cf_realloc(v->vector, v->count * v->alloc_cnt);
+		v->alloc_cnt = v->count;
+	}
+
+	VECTOR_UNLOCK(v);
+}
+
+
+//==========================================================
+// Local helpers - variable key size public API.
+//
+
+static bool
+vector_resize(cf_vector *v, uint32_t new_count)
+{
+	if (v->flags & VECTOR_FLAG_BIGRESIZE) {
+		if (new_count < 50) {
+			new_count = 50;
+		}
+	}
+	else if (new_count == 0) {
+		new_count = 2;
+	}
+
+	uint8_t *p;
+
+	if (v->vector == 0 || ! (v->flags & VECTOR_FLAG_FREE_VECTOR)) {
+		p = cf_malloc(new_count * v->ele_sz);
+
+		if (! p)	{
+			return false;
+		}
+
+		if (v->vector) {
+			memcpy(p, v->vector, v->alloc_cnt * v->ele_sz);
+			v->flags |= VECTOR_FLAG_FREE_VECTOR;
+		}
+	}
+	else {
+		p = cf_realloc(v->vector, new_count * v->ele_sz);
+	}
+
+	if (! p) {
+		return false;
+	}
+
+	v->vector = p;
+
+	if (v->flags & VECTOR_FLAG_INITZERO) {
+		memset(v->vector + v->alloc_cnt * v->ele_sz, 0,
+				(new_count - v->alloc_cnt) * v->ele_sz);
+	}
+
+	v->alloc_cnt = new_count;
+
+	return true;
 }
