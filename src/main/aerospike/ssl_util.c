5b459384e6bd98b0fe39e63ab1604c0359ae9507
Brian Nichols <bnichols@aerospike.com>
2018-04-26 19:19:24 -0700

    In ssl_util.c, rename symbols with apr prefix and make functions static.

--- src/main/aerospike/ssl_util.c
+++ src/main/aerospike/ssl_util.c
@@ -74,21 +74,7 @@ typedef struct {
     char * 	elts;
 } apr_array_header_t;
 
-
-typedef struct {
-    char * vhost_id;
-} SSLSrvConfigRec;
-
-SSLSrvConfigRec s_srv_conf_rec = { "" };
-
-#define mySrvConfig(s) (&s_srv_conf_rec)
-
-void * apr_palloc(apr_pool_t * pp, size_t sz)
-{
-	return cf_malloc(sz);
-}
-
-apr_array_header_t * apr_array_make(apr_pool_t * pp,
+static apr_array_header_t * as_apr_array_make(apr_pool_t * pp,
                                     size_t nelts,
                                     size_t eltsz)
 {
@@ -105,7 +91,7 @@ apr_array_header_t * apr_array_make(apr_pool_t * pp,
     return arr;
 }
 
-void array_destroy(apr_array_header_t * arr)
+static void as_array_destroy(apr_array_header_t * arr)
 {
 	for (int ii = 0; ii < arr->nelts; ++ii) {
 		char ** ptr = (char **) (arr->elts + (ii * arr->elt_size));
@@ -117,7 +103,7 @@ void array_destroy(apr_array_header_t * arr)
 	cf_free(arr);
 }
 
-void * apr_array_push(apr_array_header_t * arr)
+static void * as_apr_array_push(apr_array_header_t * arr)
 {
 	// If we don't have enough space double up.
 	if (arr->nelts == arr->nalloc) {
@@ -139,190 +125,15 @@ void * apr_array_push(apr_array_header_t * arr)
     return ptr;
 }
 
-#define 	APR_ARRAY_PUSH(ary, type)   (*((type *)apr_array_push(ary)))
+#define 	APR_ARRAY_PUSH(ary, type)   (*((type *)as_apr_array_push(ary)))
 
-int apr_is_empty_array(const apr_array_header_t * arr)
+static int as_apr_is_empty_array(const apr_array_header_t * arr)
 {
 	return (arr->nelts == 0);
 }
 
 // On non-EBCDIC system, this function is replaced by an empty macro.
 #define ap_xlate_proto_from_ascii(buff, len)
-
-const char * ap_strchr_c(const char * str, int c)
-{
-	return strchr(str, c);
-}
-
-#if 0
-
-#include "ssl_private.h"
-
-/*  _________________________________________________________________
-**
-**  Additional High-Level Functions for OpenSSL
-**  _________________________________________________________________
-*/
-
-/* we initialize this index at startup time
- * and never write to it at request time,
- * so this static is thread safe.
- * also note that OpenSSL increments at static variable when
- * SSL_get_ex_new_index() is called, so we _must_ do this at startup.
- */
-static int app_data2_idx = -1;
-
-void modssl_init_app_data2_idx(void)
-{
-    int i;
-
-    if (app_data2_idx > -1) {
-        return;
-    }
-
-    /* we _do_ need to call this twice */
-    for (i = 0; i <= 1; i++) {
-        app_data2_idx =
-            SSL_get_ex_new_index(0,
-                                 "Second Application Data for SSL",
-                                 NULL, NULL, NULL);
-    }
-}
-
-void *modssl_get_app_data2(SSL *ssl)
-{
-    return (void *)SSL_get_ex_data(ssl, app_data2_idx);
-}
-
-void modssl_set_app_data2(SSL *ssl, void *arg)
-{
-    SSL_set_ex_data(ssl, app_data2_idx, (char *)arg);
-    return;
-}
-
-/*  _________________________________________________________________
-**
-**  High-Level Private Key Loading
-**  _________________________________________________________________
-*/
-
-EVP_PKEY *modssl_read_privatekey(const char* filename, EVP_PKEY **key, pem_password_cb *cb, void *s)
-{
-    EVP_PKEY *rc;
-    BIO *bioS;
-    BIO *bioF;
-
-    /* 1. try PEM (= DER+Base64+headers) */
-    if ((bioS=BIO_new_file(filename, "r")) == NULL)
-        return NULL;
-    rc = PEM_read_bio_PrivateKey(bioS, key, cb, s);
-    BIO_free(bioS);
-
-    if (rc == NULL) {
-        /* 2. try DER+Base64 */
-        if ((bioS = BIO_new_file(filename, "r")) == NULL)
-            return NULL;
-
-        if ((bioF = BIO_new(BIO_f_base64())) == NULL) {
-            BIO_free(bioS);
-            return NULL;
-        }
-        bioS = BIO_push(bioF, bioS);
-        rc = d2i_PrivateKey_bio(bioS, NULL);
-        BIO_free_all(bioS);
-
-        if (rc == NULL) {
-            /* 3. try plain DER */
-            if ((bioS = BIO_new_file(filename, "r")) == NULL)
-                return NULL;
-            rc = d2i_PrivateKey_bio(bioS, NULL);
-            BIO_free(bioS);
-        }
-    }
-    if (rc != NULL && key != NULL) {
-        if (*key != NULL)
-            EVP_PKEY_free(*key);
-        *key = rc;
-    }
-    return rc;
-}
-
-/*  _________________________________________________________________
-**
-**  Smart shutdown
-**  _________________________________________________________________
-*/
-
-int modssl_smart_shutdown(SSL *ssl)
-{
-    int i;
-    int rc;
-    int flush;
-
-    /*
-     * Repeat the calls, because SSL_shutdown internally dispatches through a
-     * little state machine. Usually only one or two interation should be
-     * needed, so we restrict the total number of restrictions in order to
-     * avoid process hangs in case the client played bad with the socket
-     * connection and OpenSSL cannot recognize it.
-     */
-    rc = 0;
-    flush = !(SSL_get_shutdown(ssl) & SSL_SENT_SHUTDOWN);
-    for (i = 0; i < 4 /* max 2x pending + 2x data = 4 */; i++) {
-        rc = SSL_shutdown(ssl);
-        if (rc >= 0 && flush && (SSL_get_shutdown(ssl) & SSL_SENT_SHUTDOWN)) {
-            /* Once the close notity is sent through the output filters,
-             * ensure it is flushed through the socket.
-             */
-            if (BIO_flush(SSL_get_wbio(ssl)) <= 0) {
-                rc = -1;
-                break;
-            }
-            flush = 0;
-        }
-        if (rc != 0)
-            break;
-    }
-    return rc;
-}
-
-/*  _________________________________________________________________
-**
-**  Certificate Checks
-**  _________________________________________________________________
-*/
-
-/* retrieve basic constraints ingredients */
-BOOL modssl_X509_getBC(X509 *cert, int *ca, int *pathlen)
-{
-    BASIC_CONSTRAINTS *bc;
-    BIGNUM *bn = NULL;
-    char *cp;
-
-    bc = X509_get_ext_d2i(cert, NID_basic_constraints, NULL, NULL);
-    if (bc == NULL)
-        return FALSE;
-    *ca = bc->ca;
-    *pathlen = -1 /* unlimited */;
-    if (bc->pathlen != NULL) {
-        if ((bn = ASN1_INTEGER_to_BN(bc->pathlen, NULL)) == NULL) {
-            BASIC_CONSTRAINTS_free(bc);
-            return FALSE;
-        }
-        if ((cp = BN_bn2dec(bn)) == NULL) {
-            BN_free(bn);
-            BASIC_CONSTRAINTS_free(bc);
-            return FALSE;
-        }
-        *pathlen = atoi(cp);
-        OPENSSL_free(cp);
-        BN_free(bn);
-    }
-    BASIC_CONSTRAINTS_free(bc);
-    return TRUE;
-}
-
-#endif
 
 /* convert an ASN.1 string to a UTF-8 string (escaping control characters) */
 static char *asn1_string_to_utf8(apr_pool_t *p, ASN1_STRING *asn1str)
@@ -338,7 +149,7 @@ static char *asn1_string_to_utf8(apr_pool_t *p, ASN1_STRING *asn1str)
                                        ASN1_STRFLGS_UTF8_CONVERT);
     len = BIO_pending(bio);
     if (len > 0) {
-        result = apr_palloc(p, len+1);
+		result = cf_malloc(len+1);
         len = BIO_read(bio, result, len);
         result[len] = NUL;
     }
@@ -347,48 +158,12 @@ static char *asn1_string_to_utf8(apr_pool_t *p, ASN1_STRING *asn1str)
 }
 
 /* convert a NAME_ENTRY to UTF8 string */
-char *modssl_X509_NAME_ENTRY_to_string(apr_pool_t *p, X509_NAME_ENTRY *xsne)
+static char *modssl_X509_NAME_ENTRY_to_string(apr_pool_t *p, X509_NAME_ENTRY *xsne)
 {
     char *result = asn1_string_to_utf8(p, X509_NAME_ENTRY_get_data(xsne));
     ap_xlate_proto_from_ascii(result, len);
     return result;
 }
-
-#if 0
-
-/*
- * convert an X509_NAME to an RFC 2253 formatted string, optionally truncated
- * to maxlen characters (specify a maxlen of 0 for no length limit)
- */
-char *modssl_X509_NAME_to_string(apr_pool_t *p, X509_NAME *dn, int maxlen)
-{
-    char *result = NULL;
-    BIO *bio;
-    int len;
-
-    if ((bio = BIO_new(BIO_s_mem())) == NULL)
-        return NULL;
-    X509_NAME_print_ex(bio, dn, 0, XN_FLAG_RFC2253);
-    len = BIO_pending(bio);
-    if (len > 0) {
-        result = apr_palloc(p, (maxlen > 0) ? maxlen+1 : len+1);
-        if (maxlen > 0 && maxlen < len) {
-            len = BIO_read(bio, result, maxlen);
-            if (maxlen > 2) {
-                /* insert trailing ellipsis if there's enough space */
-                apr_snprintf(result + maxlen - 3, 4, "...");
-            }
-        } else {
-            len = BIO_read(bio, result, len);
-        }
-        result[len] = NUL;
-    }
-    BIO_free(bio);
-
-    return result;
-}
-
-#endif
 
 static void parse_otherName_value(apr_pool_t *p, ASN1_TYPE *value,
                                   const char *onf, apr_array_header_t **entries)
@@ -423,7 +198,7 @@ static void parse_otherName_value(apr_pool_t *p, ASN1_TYPE *value,
  * GEN_OTHERNAME (requires the otherName form ["onf"] argument to be supplied,
  *                see parse_otherName_value for the currently supported forms)
  */
-BOOL modssl_X509_getSAN(apr_pool_t *p, X509 *x509, int type, const char *onf,
+static BOOL modssl_X509_getSAN(apr_pool_t *p, X509 *x509, int type, const char *onf,
                         int idx, apr_array_header_t **entries)
 {
     STACK_OF(GENERAL_NAME) *names;
@@ -432,7 +207,7 @@ BOOL modssl_X509_getSAN(apr_pool_t *p, X509 *x509, int type, const char *onf,
     if (!x509 || (type < GEN_OTHERNAME) ||
         ((type == GEN_OTHERNAME) && (nid == NID_undef)) ||
         (type > GEN_RID) || (idx < -1) ||
-        !(*entries = apr_array_make(p, 0, sizeof(char *)))) {
+        !(*entries = as_apr_array_make(p, 0, sizeof(char *)))) {
         *entries = NULL;
         return FALSE;
     }
@@ -486,7 +261,7 @@ BOOL modssl_X509_getSAN(apr_pool_t *p, X509 *x509, int type, const char *onf,
         sk_GENERAL_NAME_pop_free(names, GENERAL_NAME_free);
     }
 
-    return apr_is_empty_array(*entries) ? FALSE : TRUE;
+    return as_apr_is_empty_array(*entries) ? FALSE : TRUE;
 }
 
 /* return an array of (RFC 6125 coined) DNS-IDs and CN-IDs in a certificate */
@@ -509,7 +284,7 @@ static BOOL getIDs(apr_pool_t *p, X509 *x509, apr_array_header_t **ids)
             modssl_X509_NAME_ENTRY_to_string(p, X509_NAME_get_entry(subj, i));
     }
 
-    return apr_is_empty_array(*ids) ? FALSE : TRUE;
+    return as_apr_is_empty_array(*ids) ? FALSE : TRUE;
 }
 
 /* 
@@ -517,7 +292,7 @@ static BOOL getIDs(apr_pool_t *p, X509 *x509, apr_array_header_t **ids)
  * DNS-IDs and CN-IDs (RFC 6125), optionally with basic wildcard matching.
  * If server_rec is non-NULL, some (debug/trace) logging is enabled.
  */
-BOOL modssl_X509_match_name(apr_pool_t *p, X509 *x509, const char *name,
+static BOOL modssl_X509_match_name(apr_pool_t *p, X509 *x509, const char *name,
                             BOOL allow_wildcard, server_rec *s)
 {
     BOOL matched = FALSE;
@@ -557,7 +332,7 @@ BOOL modssl_X509_match_name(apr_pool_t *p, X509 *x509, const char *name,
              * to match a dot. Otherwise, try a simple string compare.
              */
             if ((allow_wildcard == TRUE && is_wildcard == TRUE &&
-                 (cp = ap_strchr_c(name, '.')) && !strcasecmp(id[i]+1, cp)) ||
+                 (cp = strchr(name, '.')) && !strcasecmp(id[i]+1, cp)) ||
                 !strcasecmp(id[i], name)) {
                 matched = TRUE;
             }
@@ -587,7 +362,7 @@ BOOL modssl_X509_match_name(apr_pool_t *p, X509 *x509, const char *name,
 	}
 #endif
 
-    array_destroy(ids);
+    as_array_destroy(ids);
     
     return matched;
 }
@@ -603,61 +378,3 @@ bool as_tls_match_name(X509 *x509, const char *name, bool allow_wildcard)
 								  allow_wildcard,
 								  (server_rec *) NULL);
 }
-
-#if 0
-
-/*  _________________________________________________________________
-**
-**  Custom (EC)DH parameter support
-**  _________________________________________________________________
-*/
-
-DH *ssl_dh_GetParamFromFile(const char *file)
-{
-    DH *dh = NULL;
-    BIO *bio;
-
-    if ((bio = BIO_new_file(file, "r")) == NULL)
-        return NULL;
-    dh = PEM_read_bio_DHparams(bio, NULL, NULL, NULL);
-    BIO_free(bio);
-    return (dh);
-}
-
-#ifdef HAVE_ECC
-EC_GROUP *ssl_ec_GetParamFromFile(const char *file)
-{
-    EC_GROUP *group = NULL;
-    BIO *bio;
-
-    if ((bio = BIO_new_file(file, "r")) == NULL)
-        return NULL;
-    group = PEM_read_bio_ECPKParameters(bio, NULL, NULL, NULL);
-    BIO_free(bio);
-    return (group);
-}
-#endif
-
-/*  _________________________________________________________________
-**
-**  Session Stuff
-**  _________________________________________________________________
-*/
-
-char *modssl_SSL_SESSION_id2sz(IDCONST unsigned char *id, int idlen,
-                               char *str, int strsize)
-{
-    if (idlen > SSL_MAX_SSL_SESSION_ID_LENGTH)
-        idlen = SSL_MAX_SSL_SESSION_ID_LENGTH;
-        
-    /* We must ensure not to process more than what would fit in the
-     * destination buffer, including terminating NULL */
-    if (idlen > (strsize-1) / 2)
-        idlen = (strsize-1) / 2;
-
-    ap_bin2hex(id, idlen, str);
-
-    return str;
-}
-
-#endif
