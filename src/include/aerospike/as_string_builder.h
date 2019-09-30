1a1498c840d30e7cb0ca01222da254c20bf0b4e9
Brian Nichols <bnichols@aerospike.com>
2019-09-30 11:25:43 -0700

    Reformat comments in as_string_builder.h

--- src/include/aerospike/as_string_builder.h
+++ src/include/aerospike/as_string_builder.h
@@ -23,45 +23,45 @@ extern "C" {
 #endif
 
 /******************************************************************************
- *	TYPES
+ * TYPES
  ******************************************************************************/
 
 /**
- *	Fast, non thread safe string builder implementation.
+ * Fast, non thread safe string builder implementation.
  */
 typedef struct as_string_builder_s {
 	/**
-	 *	String Buffer
+	 * String Buffer
 	 */
 	char* data;
 
 	/**
-	 *	Number of bytes allocated to the buffer
+	 * Number of bytes allocated to the buffer
 	 */
 	uint32_t capacity;
 
 	/**
-	 *	String length of buffer.
+	 * String length of buffer.
 	 */
 	uint32_t length;
 	
 	/**
-	 *	Allow resize.
+	 * Allow resize.
 	 */
 	bool resize;
 	
 	/**
-	 *	Should buffer be freed on destroy.
+	 * Should buffer be freed on destroy.
 	 */
 	bool free;
 } as_string_builder;
 
 /******************************************************************************
- *	MACROS
+ * MACROS
  ******************************************************************************/
 	
 /**
- *	Initialize string builder with a stack allocated buffer.
+ * Initialize string builder with a stack allocated buffer.
  */
 #define as_string_builder_inita(__sb, __capacity, __resize)\
 (__sb)->data = alloca(__capacity);\
@@ -72,17 +72,17 @@ typedef struct as_string_builder_s {
 (__sb)->free = false;
 	
 /******************************************************************************
- *	INSTANCE FUNCTIONS
+ * INSTANCE FUNCTIONS
  ******************************************************************************/
 	
 /**
- *	Initialize string builder with a heap allocated buffer.
+ * Initialize string builder with a heap allocated buffer.
  */
 AS_EXTERN void
 as_string_builder_init(as_string_builder* sb, uint32_t capacity, bool resize);
 
 /**
- *	Assign external buffer to string builder.  External buffers are not resized.
+ * Assign external buffer to string builder.  External buffers are not resized.
  */
 static inline void
 as_string_builder_assign(as_string_builder* sb, uint32_t buffer_size, char* buffer)
@@ -96,14 +96,14 @@ as_string_builder_assign(as_string_builder* sb, uint32_t buffer_size, char* buff
 }
 
 /**
- *	Free the resources allocated to the buffer.
+ * Free the resources allocated to the buffer.
  */
 AS_EXTERN void
 as_string_builder_destroy(as_string_builder* sb);
 
 /**
- *	Initialize to empty string from current state.
- *	Capacity remains unchanged.
+ * Initialize to empty string from current state.
+ * Capacity remains unchanged.
  */
 static inline void
 as_string_builder_reset(as_string_builder* sb)
@@ -113,29 +113,29 @@ as_string_builder_reset(as_string_builder* sb)
 }
 
 /**
- *	Append null terminated string value to string buffer.
- *	Returns if successful or not.
+ * Append null terminated string value to string buffer.
+ * Returns if successful or not.
  */
 AS_EXTERN bool
 as_string_builder_append(as_string_builder* sb, const char* value);
 
 /**
- *	Append a single character to string buffer.
- *	Returns if successful or not.
+ * Append a single character to string buffer.
+ * Returns if successful or not.
  */
 AS_EXTERN bool
 as_string_builder_append_char(as_string_builder* sb, char value);
 
 /**
- *	Append bytes in hex format to string buffer.
- *	Returns if successful or not.
+ * Append bytes in hex format to string buffer.
+ * Returns if successful or not.
  */
 AS_EXTERN bool
 as_string_builder_append_bytes(as_string_builder* sb, uint8_t* src, uint32_t size);
 
 /**
- *	Append integer to string buffer.
- *	Returns if successful or not.
+ * Append integer to string buffer.
+ * Returns if successful or not.
  */
 AS_EXTERN bool
 as_string_builder_append_int(as_string_builder* sb, int val);
