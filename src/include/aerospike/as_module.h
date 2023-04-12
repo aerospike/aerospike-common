d86b25ab3fd9016d131cee3ab8de40fdc37f305e
Brian Nichols <bnichols@aerospike.com>
2023-04-12 14:31:53 -0700

    Fix doxygen missing parameter or misnamed parameter errors in as_module.h Reformat as_module.h according to standards.

--- src/include/aerospike/as_module.h
+++ src/include/aerospike/as_module.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2008-2018 Aerospike, Inc.
+ * Copyright 2008-2023 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
@@ -27,9 +27,9 @@
 extern "C" {
 #endif
 
-/*****************************************************************************
- * TYPES
- *****************************************************************************/
+//---------------------------------
+// Types
+//---------------------------------
 
 struct as_module_s;
 
@@ -40,32 +40,31 @@ struct as_module_s;
  * e.type = AS_MODULE_CONFIGURE;
  * e.data.config = my_config;
  */
-
 typedef enum as_module_event_type_e {
-    AS_MODULE_EVENT_CONFIGURE     = 0,
-    AS_MODULE_EVENT_FILE_SCAN     = 1,
-    AS_MODULE_EVENT_FILE_ADD      = 2,
-    AS_MODULE_EVENT_FILE_REMOVE   = 3,
-    AS_MODULE_EVENT_CLEAR_CACHE   = 4
+	AS_MODULE_EVENT_CONFIGURE     = 0,
+	AS_MODULE_EVENT_FILE_SCAN     = 1,
+	AS_MODULE_EVENT_FILE_ADD      = 2,
+	AS_MODULE_EVENT_FILE_REMOVE   = 3,
+	AS_MODULE_EVENT_CLEAR_CACHE   = 4
 } as_module_event_type;
 
 typedef struct as_module_event_data_s {
-    void * 			config;
-    const char * 	filename;
+	void* config;
+	const char* filename;
 } as_module_event_data;
 
 typedef struct as_module_event_s {
-    as_module_event_type     type;
-    as_module_event_data     data;
+    as_module_event_type type;
+    as_module_event_data data;
 } as_module_event;
 
 typedef struct as_module_error_s {
-	uint8_t		scope;
-	uint32_t 	code;
-	char 		message[1024];
-	char 		file[256];
-	uint32_t	line;
-	char 		func[256];
+	uint8_t scope;
+	uint32_t code;
+	char message[1024];
+	char file[256];
+	uint32_t line;
+	char func[256];
 } as_module_error;
 
 /**
@@ -73,58 +72,56 @@ typedef struct as_module_error_s {
  * Provide functions which interface with a module.
  */
 typedef struct as_module_hooks_s {
+	/**
+	 * Free resources used by the module.
+	 */
+	int (*destroy)(struct as_module_s* m);
 
-    /**
-     * Free resources used by the module.
-     */
-    int (* destroy)(struct as_module_s * m);
+	/**
+	 * Dispatch an event to the module.
+	 */
+	int (*update)(struct as_module_s* m, as_module_event* e);
 
-    /**
-     * Dispatch an event to the module.
-     */
-    int (* update)(struct as_module_s * m, as_module_event * e);
+	/**
+	 * Apply a functio to a record
+	 */
+	int (*validate)(struct as_module_s* m, as_aerospike* as, const char* filename,
+		const char* content, uint32_t size, as_module_error* err);
 
-    /**
-     * Apply a functio to a record
-     */
-    int (* validate)(struct as_module_s * m, as_aerospike * as, const char * filename, const char * content, uint32_t size, as_module_error * err);
+	/**
+	 * Apply a function to a record
+	 */
+	int (*apply_record)(struct as_module_s* m, as_udf_context* ctx, const char* filename,
+		const char* function, as_rec* rec, as_list* args, as_result* res);
 
-    /**
-     * Apply a function to a record
-     */
-    int (* apply_record)(struct as_module_s * m, as_udf_context *ctx, const char * filename, const char * function, as_rec * rec, as_list * args, as_result * res);
-
-    /**
-     * Apply a function to a stream.
-     */
-    int (* apply_stream)(struct as_module_s * m, as_udf_context *ctx, const char * filename, const char * function, as_stream * istream, as_list * args, as_stream * ostream, as_result *res);
-
+	/**
+	 * Apply a function to a stream.
+	 */
+	int (*apply_stream)(struct as_module_s* m, as_udf_context* ctx, const char* filename,
+		const char* function, as_stream* istream, as_list* args, as_stream* ostream,
+		as_result* res);
 } as_module_hooks;
 
 /**
- * Module Structure.
- * Contains pointer to module specific data and a pointer to the
+ * Module Structure. Contains pointer to module specific data and a pointer to the
  * hooks that interface with the module.
- *
- * @field source contains module specific data.
- * @field hooks contains functions that can be applied to the module.
  */
 typedef struct as_module_s {
-    const void *            source;
-    const as_module_hooks * hooks;
+	const void* source;
+	const as_module_hooks* hooks;
 } as_module;
 
-
-/*****************************************************************************
- * INLINE FUNCTIONS
- *****************************************************************************/
+//---------------------------------
+// Functions
+//---------------------------------
 
 /**
  * Get the source of the module.
  *
  * @param m the module to get the source from.
  */
-void * as_module_source(as_module * m);
+void*
+as_module_source(as_module* m);
 
 /**
  * Module Destroyer.
@@ -135,7 +132,8 @@ void * as_module_source(as_module * m);
  * @param m the module being initialized.
  * @return 0 on success, otherwise 1
  */
-int as_module_destroy(as_module * m);
+int
+as_module_destroy(as_module* m);
 
 /**
  * Module Configurator. 
@@ -145,9 +143,11 @@ int as_module_destroy(as_module * m);
  * Proxies to `m->hooks->configure(m, ...)`
  *
  * @param m the module being configured.
- * @return 0 on success, otherwhise 1
+ * @param c the new configurations.
+ * @return 0 on success, otherwise 1
  */
-int as_module_configure(as_module * m, void * c);
+int
+as_module_configure(as_module* m, void* c);
 
 /**
  * Update a Module.
@@ -155,9 +155,11 @@ int as_module_configure(as_module * m, void * c);
  * Proxies to `m->hooks->update(m, ...)`
  *
  * @param m the module being initialized.
+ * @param e the module event.
  * @return 0 on success, otherwise 1
  */
-int as_module_update(as_module * m, as_module_event * e);
+int
+as_module_update(as_module* m, as_module_event* e);
 
 /**
  * Validates a UDF provided by a string.
@@ -166,11 +168,12 @@ int as_module_update(as_module * m, as_module_event * e);
  * @param as 			aerospike object to be used.
  * @param filename 		The name of the udf module to be validated.
  * @param content 		The content of the udf module to be validated.
+ * @param size			The size of the udf module to be validated.
  * @param error 		The error string (1024 bytes). Should be preallocated. Will be an empty string if no error occurred.
  *
  * @return 0 on success, otherwise 1 on error.
  */
-int as_module_validate(as_module * m, as_aerospike * as, const char * filename, const char * content, uint32_t size, as_module_error * error);
+int as_module_validate(as_module* m, as_aerospike* as, const char* filename, const char* content, uint32_t size, as_module_error* error);
 
 /**
  * Applies a UDF to a stream with provided arguments.
@@ -181,11 +184,13 @@ int as_module_validate(as_module * m, as_aerospike * as, const char * filename, 
  * @param function		The name of the udf function to be executed.
  * @param r 			record to apply to the function.
  * @param args 			list of arguments for the function represented as vals 
- * @param result 		pointer to a val that will be populated with the result.
+ * @param res			pointer to a val that will be populated with the result.
  *
  * @return 0 on success, otherwise 1
  */
-int as_module_apply_record(as_module * m, as_udf_context * ctx, const char * filename, const char * function, as_rec * r, as_list * args, as_result * res);
+int
+as_module_apply_record(as_module* m, as_udf_context* ctx, const char* filename,
+	const char* function, as_rec* r, as_list* args, as_result* res);
 
 /**
  * Applies function to a stream and set of arguments. Pushes the results into an output stream.
@@ -199,18 +204,21 @@ int as_module_apply_record(as_module * m, as_udf_context * ctx, const char * fil
  * @param istream 		pointer to a readable stream, that will provides values.
  * @param args 			list of arguments for the function represented as vals 
  * @param ostream 		pointer to a writable stream, that will be populated with results.
- * @param result 		pointer to a val that will be populated with the result.
+ * @param res			pointer to a val that will be populated with the result.
  *
  * @return 0 on success, otherwise 1
  */
-int as_module_apply_stream(as_module * m, as_udf_context * ctx, const char * filename, const char * function, as_stream * istream, as_list * args, as_stream * ostream, as_result *res);
+int
+as_module_apply_stream(as_module* m, as_udf_context* ctx, const char* filename,
+	const char* function, as_stream* istream, as_list* args, as_stream* ostream, as_result* res);
 
 /**
  * Return lua error in string format when error code is passed in
  *
- * @param errno        The error code
+ * @param e				The error code
  */
-char *as_module_err_string(int);
+char*
+as_module_err_string(int e);
 
 #ifdef __cplusplus
 } // end extern "C"
